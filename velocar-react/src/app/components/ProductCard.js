b53622451f784be737fae8dbbac1a030c3175c89
ali ilan <ali.ilan@ozu.edu.tr>
2024-05-25 21:26:53 +0300

    minor changes.

--- velocar-react/src/app/components/ProductCard.js
+++ velocar-react/src/app/components/ProductCard.js
@@ -16,10 +16,8 @@ function ProductCard({ product }) {
     <Card>
       <CardImg top width="100%" src={product.imageUrl} alt={product.title} />
       <CardBody>
-        <CardTitle tag="h5">{product.title}</CardTitle>
-        <CardSubtitle tag="h6" className="mb-2 text-muted">
-          {product.price}
-        </CardSubtitle>
+        <CardTitle>{product.title}</CardTitle>
+        <CardSubtitle className="mb-2 text-muted">{product.price}</CardSubtitle>
         <CardText>{product.gear}</CardText>
         <Button href={"../pages/" + product.id}>View Details</Button>
       </CardBody>
