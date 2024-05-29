98854574ae427f5c1d0df21bf795462a1a98d0ee
Eren Darak <eren.darak@ozu.edu.tr>
2024-05-29 20:47:49 +0300

    Made classes component based

---
+++ velocar-react/src/app/components/Faq.js
@@ -0,0 +1,134 @@
+'use client'
+import '../public/assets/styles/carView.css';
+import Layout from '../components/Layout';
+import React, { useState } from "react";
+
+const containsNumbers = (str) => /\d/.test(str);
+
+const validateIfInvalidName = (name) => {
+  if (name.trim() === "" || containsNumbers(name)) {
+    return false;
+  }
+  return true;
+};
+
+export default function Faq() {
+
+    const [subscriber, setSubscriber] = useState({ name: '', email: '' });
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [error, setError] = useState(null);
+    const [successMessage, setSuccessMessage] = useState(null);
+
+    const handleInputChange = (e) => {
+        const { id, value } = e.target;
+        setSubscriber({ ...subscriber, [id]: value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setIsSubmitting(true);
+        setError(null);
+        setSuccessMessage(null);
+
+        if (!validateIfInvalidName(subscriber.name)) {
+            setError('Please enter a valid name without numbers.');
+            setIsSubmitting(false);
+            return;
+        }
+
+        try {
+            const response = await fetch('http://localhost:3001/subscribers?email=' + subscriber.email);
+            const data = await response.json();
+
+            if (data.length > 0) {
+                const existingSubscriber = data[0];
+                await updateSubscriber(existingSubscriber.id, { ...existingSubscriber, name: subscriber.name });
+                setSuccessMessage('Name updated successfully.');
+            } else {
+                const postResponse = await fetch('http://localhost:3001/subscribers', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(subscriber)
+                });
+                if (postResponse.ok) {
+                    setSuccessMessage('Subscriber saved successfully!');
+                    setSubscriber({ name: '', email: '' });
+                } else {
+                    setError('An error occurred while saving the subscriber.');
+                }
+            }
+            setIsSubmitting(false);
+        } catch (error) {
+            setError('An error occurred. Please try again.');
+            console.error('Error:', error);
+            setIsSubmitting(false);
+        }
+    };
+
+    const updateSubscriber = async (id, updatedData) => {
+        const response = await fetch(`http://localhost:3001/subscribers/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatedData),
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to update subscriber');
+        }
+
+        return response.json();
+    };
+
+    return (
+        <Layout>
+            <main className="container">
+                <h1 className="font">FAQ</h1>
+                <div className="grid-container background-color">
+                    <div className="grid-image">
+                        <div className="image-container">
+                            <h2 className="VeloCarLogo font">How it Works?</h2>
+                            <p className="font3">
+                                Buying with VeloCars is as easy as 1,2,3. Simply search for your car, pay online or use our finance or trade-in options, and then have the car delivered to one of our Customer Centers. We are the hassle-free, fast and secure way to buy yourself a used car, and we're by your side every step of the way.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="grid-info">
+                        <h2 className="VeloCarLogo font">Wanna learn more about our business?</h2>
+                        <p className="font3">For contacting us, please:</p>
+                        <form className="allign" onSubmit={handleSubmit}>
+                            <label className="font2">Name</label>
+                            <input
+                                type="text"
+                                className="largerInput"
+                                id="name"
+                                placeholder="Example Example"
+                                value={subscriber.name}
+                                onChange={handleInputChange}
+                                required
+                            />
+                            <label className="font2">E-Mail</label>
+                            <input
+                                type="email"
+                                className="largerInput"
+                                id="email"
+                                placeholder="example@gmail.com"
+                                value={subscriber.email}
+                                onChange={handleInputChange}
+                                required
+                            />
+                            <button type="submit" className="buy-button" disabled={isSubmitting}>
+                                {isSubmitting ? 'Sending...' : 'Send'}
+                            </button>
+                        </form>
+                        {error && <p className="error-message">{error}</p>}
+                        {successMessage && <p className="success-message">{successMessage}</p>}
+                    </div>
+                </div>
+            </main>
+        </Layout>
+    );
+}
---
+++ velocar-react/src/app/components/Payment.js
@@ -0,0 +1,232 @@
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import "../public/assets/styles/payment.css";
+import Layout from "../components/Layout";
+
+const Payment = () => {
+  const router = useRouter();
+
+  const [paymentInfo, setPaymentInfo] = useState({
+    cardName: "",
+    cardNumber: "",
+    cvv: "",
+    expDate: "",
+    paymentMethod: "Credit Card",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPaymentInfo({
+      ...paymentInfo,
+      [name]: value,
+    });
+  };
+
+  const handleCardNumberInput = (e) => {
+    const sanitizedValue = e.target.value.replace(/\D/g, "").slice(0, 16);
+    setPaymentInfo((prevState) => ({
+      ...prevState,
+      cardNumber: sanitizedValue,
+    }));
+  };
+
+  const handleCvvInput = (e) => {
+    const sanitizedValue = e.target.value.replace(/\D/g, "").slice(0, 3);
+    setPaymentInfo((prevState) => ({ ...prevState, cvv: sanitizedValue }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!validate()) {
+      resetForm();
+      return;
+    }
+
+    try {
+      const response = await fetch("http://localhost:3001/payments", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(paymentInfo),
+      });
+
+      if (response.ok) {
+        alert("Payment completed succesfully!");
+        resetForm();
+        router.push("/");
+      } else {
+        const errorData = await response.json();
+        console.error("Error response:", errorData);
+        alert("Payment Submission Failed");
+      }
+    } catch (error) {
+      console.error("Fetch error:", error);
+      alert("Payment Submission Failed");
+    }
+  };
+
+  const validate = () => {
+    const { cardName, cardNumber, cvv, expDate } = paymentInfo;
+
+    if (cardName === "" || cardNumber === "" || cvv === "" || expDate === "") {
+      alert("Please fill in all fields.");
+      return false;
+    }
+
+    if (!validateIfInvalidName(cardName)) {
+      return false;
+    }
+
+    const today = new Date();
+    const expirationDate = new Date(expDate);
+
+    if (expirationDate <= today) {
+      alert("Expiration date has already passed.");
+      return false;
+    }
+
+    if (cvv.length !== 3) {
+      alert("CVV must be exactly 3 digits long.");
+      return false;
+    }
+
+    if (cardNumber.length !== 16) {
+      alert("Card Number must be exactly 16 characters long.");
+      return false;
+    }
+
+    return true;
+  };
+
+  const containsNumbers = (value) => {
+    return /\d/.test(value);
+  };
+
+  const validateIfInvalidName = (name) => {
+    if (name.trim() === "" || containsNumbers(name)) {
+      alert("Please enter a valid name without numbers.");
+      return false;
+    }
+    return true;
+  };
+
+  const resetForm = () => {
+    setPaymentInfo({
+      cardName: "",
+      cardNumber: "",
+      cvv: "",
+      expDate: "",
+      paymentMethod: "Credit Card",
+    });
+  };
+
+  return (
+    <Layout>
+      <div className="PaymentPage">
+        <div className="PaymentMethodBar">
+          <h1 className="choosePaymentText">
+            <u>Choose Your Payment Method</u>
+          </h1>
+          <div className="radioButtonCard">
+            <input
+              className="radioButton"
+              type="radio"
+              id="CreditCard"
+              name="paymentMethod"
+              value="Credit Card"
+              checked={paymentInfo.paymentMethod === "Credit Card"}
+              onChange={handleChange}
+            />
+            <label htmlFor="CreditCard">Credit Card</label>
+          </div>
+
+          <div className="radioButtonDebitCard">
+            <input
+              className="radioButton"
+              type="radio"
+              id="Debit"
+              name="paymentMethod"
+              value="Debit Card"
+              checked={paymentInfo.paymentMethod === "Debit Card"}
+              onChange={handleChange}
+            />
+            <label htmlFor="Debit">Debit Card</label>
+          </div>
+        </div>
+
+        <div className="PaymentInfoBar">
+          <form onSubmit={handleSubmit} id="PaymentForm">
+            <div className="CardNameText">
+              <p>
+                <b>Name On the Card</b>
+              </p>
+              <input
+                className="CNameInput"
+                type="text"
+                name="cardName"
+                id="CNameInput"
+                value={paymentInfo.cardName}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="CardNum">
+              <p>
+                <b>Card Number</b>
+              </p>
+              <input
+                className="CNumInput"
+                type="text"
+                name="cardNumber"
+                id="CNumInput"
+                value={paymentInfo.cardNumber}
+                onChange={handleCardNumberInput}
+              />
+            </div>
+
+            <div className="CVV">
+              <p>
+                <b>CVV</b>
+              </p>
+              <input
+                className="CVVInput"
+                type="text"
+                name="cvv"
+                id="CVVInput"
+                value={paymentInfo.cvv}
+                onChange={handleCvvInput}
+              />
+            </div>
+
+            <div className="ExpDate">
+              <p>
+                <b>Expiration Date</b>
+              </p>
+              <input
+                className="EXPDateInput"
+                type="date"
+                name="expDate"
+                id="EXPDateInput"
+                value={paymentInfo.expDate}
+                onChange={handleChange}
+              />
+            </div>
+
+            <button
+              className="PaymentSubmitButton"
+              type="submit"
+              name="paymentSubmit"
+              id="PaymentSubmitButton"
+            >
+              Complete the Payment
+            </button>
+          </form>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Payment;
